993c594546c5e4d9605ed4c061d12bd91bbabb62
Leo <leocasti2@gmail.com>
2025-01-26 01:32:47 -0500

    feat: ResumeSerializer maneja la creación de objetos relacionados (como Skill y Experience) junto con el Resume

--- resume_app/serializers.py
+++ resume_app/serializers.py
@@ -5,13 +5,33 @@ from .models import Resume, Skill, Experience, Template
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
-        fields = "__all__"
+        fields = ["id", "name", "level", "keywords"]
+
+    def validate(self, data):
+        if "resume" not in data:
+            return data
+        return super().validate(data)
 
 
 class ExperienceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Experience
-        fields = "__all__"
+        fields = [
+            "id",
+            "name",
+            "position",
+            "url",
+            "highlights",
+            "summary",
+            "start_date",
+            "end_date",
+            "orden",
+        ]
+
+    def validate(self, data):
+        if "resume" not in data:
+            return data
+        return super().validate(data)
 
 
 class TemplateSerializer(serializers.ModelSerializer):
@@ -21,10 +41,20 @@ class TemplateSerializer(serializers.ModelSerializer):
 
 
 class ResumeSerializer(serializers.ModelSerializer):
-    skills = SkillSerializer(many=True, read_only=True)
-    experiences = ExperienceSerializer(many=True, read_only=True)
-    template_selected = TemplateSerializer(read_only=True)
+    skills = SkillSerializer(many=True, required=False)
+    experiences = ExperienceSerializer(many=True, required=False)
 
     class Meta:
         model = Resume
         fields = "__all__"
+
+    def create(self, validated_data):
+        skills_data = validated_data.pop("skills", [])
+        experiences_data = validated_data.pop("experiences", [])
+        resume = Resume.objects.create(**validated_data)
+
+        for experience_data in experiences_data:
+            Experience.objects.create(resume=resume, **experience_data)
+        for skill_data in skills_data:
+            Skill.objects.create(resume=resume, **skill_data)
+        return resume
