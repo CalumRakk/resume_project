e7790442d3feb5824c15fa6fcee135e8076586dc
Leo <leocasti2@gmail.com>
2025-01-27 06:14:43 -0500

    feat: agrega enrutamiento para resumes y agrega los componentes ResumeList y ResumeDetail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ResumeList from "./components/ResumeList";
+import ResumeDetail from "./components/ResumeDetail";
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/resumes/')
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
-
   return (
-    <div>
-      <h1>Datos desde Django:</h1>
-      <ul>
-        {data.map(item => (
-          <li key={item.id}>{item.full_name}</li>
-        ))}
-      </ul>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ResumeList />} />
+        <Route path="/resumes/:id" element={<ResumeDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/ResumeDetail.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+const apiUrl = process.env.REACT_APP_API_URL;
+
+function ResumeDetail() {
+  const { id } = useParams();
+  const [resume, setResume] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/api/resumes/${id}/`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("La respuesta no fue exitosa");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setResume(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
+  }, [id]);
+
+  if (loading) return <p>Cargando...</p>;
+  if (error) return <p>Error: {error}</p>;
+  if (!resume) return <p>Resume no encontrado</p>;
+
+  return (
+    <div>
+      <h1>{resume.full_name}</h1>
+      <p>{resume.email}</p>
+      <p>{resume.summary}</p>
+      <h2>Experiencia</h2>
+      <ul>
+        {resume.experiences.map((exp) => (
+          <li key={exp.id}>
+            <h3>{exp.name}</h3>
+            <p>{exp.position}</p>
+            <p>{exp.url}</p>
+            <p>{exp.start_date} - {exp.end_date}</p>
+            <p>{exp.summary}</p>
+          </li>
+        ))}
+      </ul>
+      <h2>Habilidades</h2>
+      <ul>
+        {resume.skills.map((skill) => (
+          <li key={skill.id}>{skill.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ResumeDetail;
