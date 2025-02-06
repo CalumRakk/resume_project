166f49054338013fbcb6f666f054e35ae76fab4e
Leo <leocasti2@gmail.com>
2025-02-06 12:51:05 -0500

    feat: actualiza ALLOWED_HOSTS

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-s(h^xt12-=)d_71e6g^9nm*sp57e=#6r54vxgn03a7puy0k#_$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["calumrakk.pythonanywhere.com"]
 
 
 # Application definition
