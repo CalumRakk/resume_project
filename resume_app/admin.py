050394bd1c1e075522559138d29a8567362f6d1b
Leo <leocasti2@gmail.com>
2025-02-07 12:05:46 -0500

    feat: mejore la interfaz de administración registrando modelos

--- resume_app/admin.py
+++ resume_app/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Resume, Skill, Experience, Template
 
-# Register your models here.
+
+class ExperienceInline(admin.TabularInline):
+    model = Experience
+    extra = 1
+
+
+class SkillInline(admin.TabularInline):
+    model = Skill
+    extra = 1
+
+
+@admin.register(Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = (
+        "full_name",
+        "email",
+        "template_selected",
+    )
+    list_filter = ("template_selected",)
+    search_fields = ("full_name", "email")
+    inlines = [
+        SkillInline,
+        ExperienceInline,
+    ]
+
+
+@admin.register(Template)
+class TemplateAdmin(admin.ModelAdmin):
+    list_display = ("name", "componet_name")
+
+
+admin.site.register(Skill)
+admin.site.register(Experience)
